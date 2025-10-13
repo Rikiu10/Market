a7c761c60c9e1a07ca2908927a6019b5f4cd7062
Rikiu_10 <ricardoporriga0@gmail.com>
2025-10-13 20:38:43 -0300

    agregue mis tablas admin.py
    se me fue jsjsj

--- Tienda/admin.py
+++ Tienda/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
 
 # Register your models here.
+
+# Tienda/admin.py
+from django.contrib import admin
+from .models import Credenciales, Movimiento, Historial
+
+@admin.register(Credenciales)
+class CredencialesAdmin(admin.ModelAdmin):
+    list_display = ("idcredenciales", "user")
+    search_fields = ("user",)
+
+@admin.register(Movimiento)
+class MovimientoAdmin(admin.ModelAdmin):
+    list_display = ("idmovimiento", "tipo", "fecha", "producto_idproducto", "descripcion")
+    list_filter  = ("tipo",)
+    search_fields = ("descripcion",)
+    date_hierarchy = "fecha"
+
+@admin.register(Historial)
+class HistorialAdmin(admin.ModelAdmin):
+    list_display = ("idhistorial", "fecha", "alertas_idalertas", "producto_idproducto")
+    date_hierarchy = "fecha"
+    search_fields = ("idhistorial",)
