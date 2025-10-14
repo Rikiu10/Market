289fc46bb58859674927bc2de8a2d34818b494ef
Rikiu_10 <ricardoporriga0@gmail.com>
2025-10-14 09:01:24 -0300

    test
    saas

--- Tienda/admin.py
+++ Tienda/admin.py
@@ -9,21 +9,26 @@ from .models import Credenciales, Movimiento, Historial, Venta
 class CredencialesAdmin(admin.ModelAdmin):
     list_display = ("idcredenciales", "user")
     search_fields = ("user",)
+    search_fields = ("user",)
 
 @admin.register(Movimiento)
 class MovimientoAdmin(admin.ModelAdmin):
     # Form del admin: solo estos campos (no pide producto_idproducto)
     fields = ("descripcion", "fecha", "tipo")
     list_display = ("idmovimiento", "tipo", "fecha", "descripcion")
+    list_display_links = ("idmovimiento", "tipo")
     list_filter  = ("tipo",)
     search_fields = ("descripcion",)
     date_hierarchy = "fecha"
+    actions_on_top = True
+    actions_on_bottom = True
 
 @admin.register(Historial)
 class HistorialAdmin(admin.ModelAdmin):
     # Form del admin: solo fecha (no pide alertas_idalertas ni producto_idproducto)
     fields = ("fecha",)
     list_display = ("idhistorial", "fecha")
+    list_display_links = ("idhistorial", "fecha") 
     date_hierarchy = "fecha"
     search_fields = ("idhistorial",)
 
